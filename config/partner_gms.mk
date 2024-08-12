2b0cbbd98082d6b2c3ba613e662da2f082c593fb
Praditia Nur <pnplusplus@gmail.com>
2024-08-12 15:17:22 +0700

    partner_gms: Rewrite to accomodate KusumaGapps
    Change-Id: Id28a196a80f15a26758b27df91f7eed79d569b1c

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -3,41 +3,30 @@ ifeq ($(WITH_GMS),true)
     ifeq ($(PRODUCT_IS_ATV),true)
         ifneq ($(GMS_MAKEFILE),)
             # Specify the GMS makefile you want to use, for example:
-            #   - gms.mk            - default Android TV GMS
-            #   - gms_gtv.mk        - default Google TV GMS
-            #   - gms_minimal.mk    - minimal Android TV GMS
-            $(call inherit-product, vendor/partner_gms-tv/products/$(GMS_MAKEFILE))
+            #   - arm
+            #   - arm64
+            $(call inherit-product, vendor/gapps/tv/$(GMS_MAKEFILE)/$(GMS_MAKEFILE)-vendor.mk)
         else
-            $(call inherit-product-if-exists, vendor/partner_gms-tv/products/gms.mk)
+            ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+                $(call inherit-product-if-exists, vendor/gapps/tv/arm64/arm64-vendor.mk)
+            else
+                $(call inherit-product-if-exists, vendor/gapps/tv/arm/arm-vendor.mk)
+            endif
         endif
-        $(call inherit-product-if-exists, vendor/partner_gms-tv/products/mainline_modules.mk)
-    # Special handling for Android Automotive
-    else ifeq ($(PRODUCT_IS_AUTO),true)
+    else
         ifneq ($(GMS_MAKEFILE),)
-            $(call inherit-product, vendor/partner_gms-car/products/$(GMS_MAKEFILE))
+            # Specify the GMS makefile you want to use, for example:
+            #   - arm
+            #   - arm64
+            #   - x86
+            #   - x86_64
+            $(call inherit-product, vendor/gapps/$(GMS_MAKEFILE)/$(GMS_MAKEFILE)-vendor.mk)
         else
-            $(call inherit-product-if-exists, vendor/partner_gms-car/products/gms.mk)
-        endif
-   else
-        # Specify the GMS makefile you want to use, for example:
-        #   - fi.mk             - Project Fi
-        #   - gms.mk            - default GMS
-        #   - gms_go.mk         - low ram devices
-        #   - gms_go_2gb.mk     - low ram devices (2GB)
-        #   - gms_64bit_only.mk - devices supporting 64-bit only
-        #   - gms_minimal.mk    - minimal GMS
-        ifneq ($(GMS_MAKEFILE),)
-            $(call inherit-product, vendor/partner_gms/products/$(GMS_MAKEFILE))
-        else
-            $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
-        endif
-
-        # Specify the mainline module makefile you want to use, for example:
-        #   - mainline_modules.mk              - updatable apex
-        #   - mainline_modules_flatten_apex.mk - flatten apex
-        #   - mainline_modules_low_ram.mk      - low ram devices
-        ifneq ($(MAINLINE_MODULES_MAKEFILE),)
-            $(call inherit-product, vendor/partner_modules/build/$(MAINLINE_MODULES_MAKEFILE))
+            ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+                $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+            else
+                $(call inherit-product-if-exists, vendor/gapps/arm/arm-vendor.mk)
+            endif
         endif
     endif
 endif
